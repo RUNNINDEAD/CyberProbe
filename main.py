479ff56b032d47de2afa0b869a31ef93c8ec6dd7
Wil Valdez <145037905+RUNNINDEAD@users.noreply.github.com>
2025-03-09 21:53:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
 # main.py
 
-import sys
-from modules import *
-from utils import *
+print ("Welcome to CyberProbe") 
 
-def main():
-    print("Welcome to the Penetration Testing Tool")
-    # Here you can add the logic to execute various modules and utilities
-    # For example:
-    # module_result = some_module_function()
-    # print(module_result)
 
-if __name__ == "__main__":
-    main()
